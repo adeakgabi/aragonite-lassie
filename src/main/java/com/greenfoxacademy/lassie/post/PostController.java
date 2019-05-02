e8de13e81981e32d80898e9ab98b9bb28280e134
Bíró Botond <biro.botond20@gmail.com>
2019-05-03 00:03:19 +0200

    refactor(PostController): modify endpoints and endpoint logic

--- src/main/java/com/greenfoxacademy/lassie/post/PostController.java
+++ src/main/java/com/greenfoxacademy/lassie/post/PostController.java
@@ -1,17 +1,18 @@
 package com.greenfoxacademy.lassie.post;
 
-import com.greenfoxacademy.lassie.exceptions.PostException;
+import com.greenfoxacademy.lassie.exception.PostException;
 import com.greenfoxacademy.lassie.post.subtypes.Found;
 import com.greenfoxacademy.lassie.post.subtypes.Lost;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller("/post")
+@RestController
 public class PostController {
 
   private PostService postService;
@@ -21,25 +22,25 @@ public class PostController {
     this.postService = postService;
   }
 
-  @GetMapping("/found/list")
-  public ResponseEntity listFoundAnimalPosts(){
+  @GetMapping("/post/found/list")
+  public ResponseEntity listFoundAnimalPosts() {
     return new ResponseEntity<>(postService.getAllFoundAnimalPosts(), HttpStatus.OK);
   }
 
-  @GetMapping("/lost/list")
-  public ResponseEntity listLostAnimalPosts(){
+  @GetMapping("/post/lost/list")
+  public ResponseEntity listLostAnimalPosts() {
     return new ResponseEntity<>(postService.getAllLostAnimalPosts(), HttpStatus.OK);
   }
 
-  @PostMapping("/found/create")
-  public ResponseEntity createFoundAnimalPost(@RequestBody Found found)
+  @PostMapping("/post/found/create")
+  public ResponseEntity createFoundAnimalPost(@RequestBody Found found, Authentication auth)
           throws PostException {
-    return new ResponseEntity<>(postService.savePost(found), HttpStatus.ACCEPTED);
+    return new ResponseEntity<>(postService.savePost(found, auth), HttpStatus.ACCEPTED);
   }
 
-  @PostMapping("/lost/create")
-  public ResponseEntity createLostAnimalPost(@RequestBody Lost lost)
+  @PostMapping("/post/lost/create")
+  public ResponseEntity createLostAnimalPost(@RequestBody Lost lost, Authentication auth)
           throws PostException {
-    return new ResponseEntity<>(postService.savePost(lost), HttpStatus.ACCEPTED);
+    return new ResponseEntity<>(postService.savePost(lost, auth), HttpStatus.ACCEPTED);
   }
 }

4293acd94fb1c65b5de431c7ab1999a713f5805c
Judit Horvath <judit.of.horvath@gmail.com>
2019-05-02 16:20:18 +0200

    feat(ApplicationUser): add comments entity

--- src/main/java/com/greenfoxacademy/lassie/user/ApplicationUser.java
+++ src/main/java/com/greenfoxacademy/lassie/user/ApplicationUser.java
@@ -1,5 +1,6 @@
 package com.greenfoxacademy.lassie.user;
 
+import com.greenfoxacademy.lassie.comment.Comment;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -22,9 +26,16 @@ public class ApplicationUser {
   private long id;
   private String username;
   private String password;
+  @OneToMany
+  private List<Comment> comments;
 
   public ApplicationUser(String username, String password){
     this.username = username;
     this.password = password;
+    this.comments = new ArrayList<>();
+  }
+  
+  public void addComment(Comment comment){
+    comments.add(comment);
   }
 }

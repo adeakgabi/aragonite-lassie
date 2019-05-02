c22a90e3dd2025611b5fabf43a8b60419d53b068
Bíró Botond <biro.botond20@gmail.com>
2019-05-02 14:50:21 +0200

    refactor(LassieApplication): modify pacakage name

--- src/main/java/com/greenfoxacademy/lassie/LassieApplication.java
+++ src/main/java/com/greenfoxacademy/lassie/LassieApplication.java
@@ -1,4 +1,4 @@
-package com.greenfoxacademy.lassie.demo;
+package com.greenfoxacademy.lassie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +9,4 @@ public class LassieApplication {
   public static void main(String[] args) {
     SpringApplication.run(LassieApplication.class, args);
   }
-
 }

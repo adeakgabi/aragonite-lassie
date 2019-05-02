4bb0bfcac6ab7ba789dad5b07ee570027ee6bad9
Deák Gabriella <adeakgabi@gmail.com>
2019-05-02 12:06:43 +0200

    feat(user): create user entity

---
+++ src/main/java/com/greenfoxacademy/lassie/demo/user/UserService.java
@@ -0,0 +1,7 @@
+package com.greenfoxacademy.lassie.demo.user;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface UserService {
+}
---
+++ src/main/java/com/greenfoxacademy/lassie/demo/user/UserServiceImpl.java
@@ -0,0 +1,4 @@
+package com.greenfoxacademy.lassie.demo.user;
+
+public class UserServiceImpl implements UserService {
+}

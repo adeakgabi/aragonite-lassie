38563629cbd4554bdc618aa44b2e4127eae86d42
Deák Gabriella <adeakgabi@gmail.com>
2019-05-02 12:19:54 +0200

    feat(userServiceImpl): add registerNewUser method

--- src/main/java/com/greenfoxacademy/lassie/demo/user/UserService.java
+++ src/main/java/com/greenfoxacademy/lassie/demo/user/UserService.java
@@ -1,7 +1,4 @@
 package com.greenfoxacademy.lassie.demo.user;
 
-import org.springframework.stereotype.Service;
-
-@Service
 public interface UserService {
 }
--- src/main/java/com/greenfoxacademy/lassie/demo/user/UserServiceImpl.java
+++ src/main/java/com/greenfoxacademy/lassie/demo/user/UserServiceImpl.java
@@ -1,4 +1,23 @@
 package com.greenfoxacademy.lassie.demo.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserServiceImpl implements UserService {
+
+  private BCryptPasswordEncoder bCryptPasswordEncoder;
+  private UserRepository userRepository;
+
+  @Autowired
+  public UserServiceImpl(BCryptPasswordEncoder bCryptPasswordEncoder, UserRepository userRepository) {
+    this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    this.userRepository = userRepository;
+  }
+
+  public ApplicationUser registerNewUser(ApplicationUser user) {
+    userRepository.save(user);
+    return user;
+  }
 }

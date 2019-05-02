a75eaee6190eea889fab8cf350bd473db8766756
Bíró Botond <biro.botond20@gmail.com>
2019-05-02 12:19:44 +0200

    refactor(LassieApplication): rename from DemoApplication to LassieApplication

---
+++ src/main/java/com/greenfoxacademy/lassie/LassieApplication.java
@@ -0,0 +1,13 @@
+package com.greenfoxacademy.lassie.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LassieApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LassieApplication.class, args);
+  }
+
+}

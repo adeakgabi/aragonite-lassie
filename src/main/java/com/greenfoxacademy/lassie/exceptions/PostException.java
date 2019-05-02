0ee6a9348ae62f4f8cdcf230e9e56b2f151a7726
Bíró Botond <biro.botond20@gmail.com>
2019-05-02 22:24:20 +0200

    feat(PostException): add PostException

---
+++ src/main/java/com/greenfoxacademy/lassie/exceptions/PostException.java
@@ -0,0 +1,10 @@
+package com.greenfoxacademy.lassie.exceptions;
+
+import com.greenfoxacademy.lassie.post.Post;
+
+public class PostException extends Exception{
+
+  public PostException(String message){
+    super(message);
+  }
+}

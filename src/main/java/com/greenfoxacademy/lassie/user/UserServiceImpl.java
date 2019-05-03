65e8b913ae07451ce4543e7ed5836c7da529dcd4
Deák Gabriella <adeakgabi@gmail.com>
2019-05-03 08:47:26 +0200

    :fire: remove useless method and unused imports

--- src/main/java/com/greenfoxacademy/lassie/user/UserServiceImpl.java
+++ src/main/java/com/greenfoxacademy/lassie/user/UserServiceImpl.java
@@ -6,15 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -60,25 +52,5 @@ public class UserServiceImpl implements UserService {
   public List<ApplicationUser> getAllUser() {
     List<ApplicationUser> users = new ArrayList<>(userRepository.findAll());
     return users;
-  }
-
-  @RequestMapping(value="/upload", method= RequestMethod.POST)
-  public @ResponseBody String handleFileUpload(
-          @RequestParam("file") MultipartFile file){
-    String name = "test11";
-    if (!file.isEmpty()) {
-      try {
-        byte[] bytes = file.getBytes();
-        BufferedOutputStream stream =
-                new BufferedOutputStream(new FileOutputStream(new File(name + "-uploaded")));
-        stream.write(bytes);
-        stream.close();
-        return "You successfully uploaded " + name + " into " + name + "-uploaded !";
-      } catch (Exception e) {
-        return "You failed to upload " + name + " => " + e.getMessage();
-      }
-    } else {
-      return "You failed to upload " + name + " because the file was empty.";
-    }
   }
 }
